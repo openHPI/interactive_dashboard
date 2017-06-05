138e76d806fee7869f2e2fa283765a78a699fd31
FWirtz <florian.wirtz08@gmail.com>
2017-06-05 15:51:33 +0200

    Add Google Maps Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { MaterializeModule } from 'angular2-materialize';
+ import { NguiMapModule} from '@ngui/map';
+
 import { AppComponent } from './app.component';
 import { KeyFeaturesComponent } from './key-features/key-features.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { FeatureCardComponent } from './feature-card/feature-card.component';
 import { PromoNumbersComponent } from './promo-numbers/promo-numbers.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { WorldMapComponent } from './world-map/world-map.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +21,15 @@ import { ReviewsComponent } from './reviews/reviews.component';
     TestimonialsComponent,
     FeatureCardComponent,
     PromoNumbersComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    WorldMapComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-	MaterializeModule
+	  MaterializeModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCQLhnDxhB2Gb7CRWLlZvWcYdEqK0GqseI'})
   ],
   providers: [],
   bootstrap: [AppComponent]
