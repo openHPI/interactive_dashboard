6a141fe11bc9f7d86fc2473c958f5b2683356d82
FWirtz <florian.wirtz08@gmail.com>
2017-07-02 22:41:25 +0200

    Add visual cues for header
    Underline the currently selected data source(s).
    
    Add variables for piping selected data later.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jenz-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  openhpi: boolean;
+  opensap: boolean;
+  moochouse: boolean;
+  openwho: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.openhpi = true;
+    this.opensap = true;
+    this.moochouse = false;
+    this.openwho = false;
   }
 
 }
