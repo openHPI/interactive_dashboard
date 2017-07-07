0d65b627b8028f7368fd050d95ad51cd483bba54
deeps96 <leonardo.huebscher.business@gmail.com>
2017-07-07 21:36:02 +0200

    Navigatorbuttons of the slider now get disabled when first/ last page displayed.
    Not possible to click click next button, when last page already
    displayed. Vice vera for first page and previous button.

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Review } from '../review';
 import { MaterializeAction } from 'angular2-materialize';
 
@@ -8,7 +8,11 @@ import { MaterializeAction } from 'angular2-materialize';
   styleUrls: ['./reviews.component.css']
 })
 
-export class ReviewsComponent{
+export class ReviewsComponent {
+
+  @ViewChildren('item') items: QueryList<any>;
+  @ViewChild('prev') prev: any;
+  @ViewChild('next') next: any;
 
   reviews: Review[] = [
 	{statement: '120 Schüler waren beim openHPI-Programmierkurs auch in ihrer Freizeit mit viel Engagement und großem Interesse bei der Sache.', imageUrl: '../assets/testimonials/square_dominik.jpg', author: 'Dominik Hausner, Informatik-Lehrer'},
@@ -17,17 +21,33 @@ export class ReviewsComponent{
 	{statement: 'openHPI offers a variety of interesting courses. Video indexation or collaboration spaces make it easier to follow and learn.', imageUrl: '../assets/testimonials/square_oriol.jpg', author: 'Oriol Borras Gene, Ph.D. Student - Technical University of Madrid'},
 	{statement: 'Hello. I\'m a random guy. This is a very long text. Bla bli blub di blub. This is a very long text. Bla bli blub di blub.', imageUrl: 'https://static.tildacdn.com/tild6231-3138-4531-b132-313863343037/28m.jpg', author: 'I\'m PROud being at the HPI :)'},
   ];
-
-  constructor() { }
   
   
   navigatorActions = new EventEmitter<string|MaterializeAction>();
 
   previousCourse() {
-    this.navigatorActions.emit({action:"carousel", params:['prev']});
+	if (this.getActiveItem() != this.items.first){
+		this.navigatorActions.emit({action:"carousel", params:['prev']});
+	}
   }
   nextCourse() {
     this.navigatorActions.emit({action:"carousel", params:['next']});
+  }
+  
+  getActiveItem(){
+	return this.items.find(this.findActiveItem);
+  }
+  
+  findActiveItem(item){
+	return item.nativeElement.classList.contains('active');
+  }  
+  
+  isFirst(){
+	return this.items ? this.getActiveItem() == this.items.first : false;
+  }
+  
+  isLast(){
+	return this.items ? this.getActiveItem() == this.items.last : false;
   }
 }
 
