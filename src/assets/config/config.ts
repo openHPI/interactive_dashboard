ceece4251d6c85c340677124e398b3b5a76d2c49
deeps96 <leonardo.huebscher.business@gmail.com>
2017-07-16 23:45:09 +0200

    Fixed config.ts. Map-markers were missing :(

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -4,6 +4,7 @@ import { OPEN_HPI_REVIEWS, OPEN_SAP_REVIEWS, MOOC_HOUSE_REVIEWS, OPEN_WHO_REVIEW
 const OPEN_HPI: Platform = {
 	rootUrl: 'https://open.hpi.de',
 	logoUrl: '../assets/logos/open_hpi_logo.png',
+	mapMarkerUrl: '../assets/marker/hpi.png',
 	reviews: OPEN_HPI_REVIEWS,
 	isFilterSelected: true
 };
@@ -11,6 +12,7 @@ const OPEN_HPI: Platform = {
 const OPEN_SAP: Platform = {
 	rootUrl: 'https://open.sap.com',
 	logoUrl: '../assets/logos/openSAP_Logo_neg.png',
+	mapMarkerUrl: '../assets/marker/sap.png',
 	reviews: OPEN_SAP_REVIEWS,
 	isFilterSelected: false
 };
@@ -18,6 +20,7 @@ const OPEN_SAP: Platform = {
 const MOOC_HOUSE: Platform = {
 	rootUrl: 'https://mooc.house',
 	logoUrl: '../assets/logos/mooc_house_logo_neg_srgb_300.png',
+	mapMarkerUrl: '../assets/marker/mooc.png',
 	reviews: MOOC_HOUSE_REVIEWS,
 	isFilterSelected: false
 };
@@ -25,6 +28,7 @@ const MOOC_HOUSE: Platform = {
 const OPEN_WHO: Platform = {
 	rootUrl: 'https://openwho.org',
 	logoUrl: '../assets/logos/who_logo_white.png',
+	mapMarkerUrl: '../assets/marker/who.png',
 	reviews: OPEN_WHO_REVIEWS,
 	isFilterSelected: false
 };
